e63e0cbe59a7822570162f5c66f11c95130de142
zjgtan <zjgtan@126.com>
2018-12-21 09:46:43 +0800

    fm

--- src/deepfm.py
+++ src/deepfm.py
@@ -10,17 +10,24 @@ class DeepFM(nn.Module):
         """
         super(DeepFM, self).__init__()
         # user_id的Embedding层
-        self.user_id_embedding_layer = nn.Embedding(num_user_id + 1, 128)
+        self.user_id_embedding_layer = nn.Embedding(num_user_id + 1, 10)
+        self.user_id_basis = nn.Embedding(num_user_id + 1, 1)
         # item_id的Embedding层
-        self.item_id_embedding_layer = nn.Embedding(num_item_id + 1, 128)
+        self.item_id_embedding_layer = nn.Embedding(num_item_id + 1, 10)
+        self.item_id_basis = nn.Embedding(num_item_id + 1, 1)
         # 全连接层
+        '''
         self.fc_layer = nn.Sequential(
-                nn.Linear(256, 256),
+                nn.Linear(40, 20),
                 nn.Sigmoid())
         # 输出层
+        #self.output_layer = nn.Sequential(
+        #        nn.Linear(200, 2),
+        #        nn.LogSoftmax())
+
         self.output_layer = nn.Sequential(
-                nn.Linear(256, 2),
-                nn.LogSoftmax())
+                nn.Linear(20, 1))
+        '''
 
     def forward(self, batch_record):
         """前向计算
@@ -32,9 +39,23 @@ class DeepFM(nn.Module):
 
         iids = batch_record["iid"]
         item_id_embedding = self.item_id_embedding_layer(iids)
+        
+        output = self.user_id_basis(uids) + \
+                self.item_id_basis(iids) + \
+                (user_id_embedding * item_id_embedding).sum(1).view(-1, 1)
 
+        output = output.view(1,-1)
+        return output
+
+        '''
         user_item_feature = torch.cat([user_id_embedding, item_id_embedding], 1)
         fc = self.fc_layer(user_item_feature)
         output = self.output_layer(fc)
 
         return output
+        '''
+
+if __name__ == "__main__":
+    record = {"uid": torch.LongTensor([1, 2]), "iid": torch.LongTensor([1, 2])}
+    obj = DeepFM(10, 10)
+    print obj.forward(record)
--- src/train.py
+++ src/train.py
@@ -1,41 +1,82 @@
 # coding: utf8
 
 import sys
-from dataset import ML1M
+from dataset import ML1MExplicit
 from torch import nn
 import torch
+import numpy as np
+import math
 
 from tensorboardX import SummaryWriter
 
 
-ml1m_dataset = ML1M("../ml-1m/")
+ml1m_dataset = ML1MExplicit("../ml-1m/")
 print >> sys.stderr, "load dataset..."
 ml1m_dataset.load_dataset()
+print >> sys.stderr, "end..."
 
 from deepfm import DeepFM
 
-model = DeepFM(6040, 3953)
+def compute_accuracy_score(preds, scores):
+    fenmu = sum(preds)
+    fenzi = sum([1 for i in range(len(preds)) if preds[i] == 1 and scores[i] == 1])
+    return fenzi * 1. / fenmu
 
-criterion = nn.NLLLoss()
+def compute_recall_score(preds, scores):
+    fenmu = sum(scores)
+    fenzi = sum([1 for i in range(len(preds)) if preds[i] == 1 and scores[i] == 1])
+    return fenzi * 1. / fenmu
+
+def evaluate(model, iter_batch):
+    scores = []
+    preds = []
+
+    for batch_records in iter_batch:
+        output = model(batch_records)
+        pred = output.data.numpy().tolist()[0]
+        score = batch_records["score"].numpy().tolist()
+
+        scores.extend(score)
+        preds.extend(pred)
+
+    fenzi = sum([(preds[i] - scores[i]) ** 2 for i in range(len(preds))])
+    fenmu = len(preds)
+    score = math.sqrt(fenzi / fenmu)
+
+    return score
+
+model = DeepFM(6040, 3953)
+#criterion = nn.NLLLoss()
+criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters())
 
 writer = SummaryWriter("log")
 niter = 0
 
-for epoch in range(10):
+batch_id = 0
+for epoch in range(100):
     for batch_id, batch_records in enumerate(ml1m_dataset.next_batch(128)):
+        #batch_records = ml1m_dataset.next_epoch()
         preds = model(batch_records)
         scores = batch_records["score"]
 
         loss = criterion(preds, scores)
-        if batch_id % 100 == 0:
+        if batch_id % 1000 == 0:
             print "iter: %d, batch_id: %d, loss: %f" % (
                     epoch, batch_id, loss.item())
-            #writer.add_scalar("Train/Loss", loss.item(), niter)
-            #niter += 1
+        #writer.add_scalar("Train/Loss", loss.item(), niter)
+        #niter += 1
         
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        #scheduler.step()
 
-    metrics = evaluate(model)
+    print "start evaluate..."
+    score = evaluate(model, ml1m_dataset.next_batch(128, True))
+    print score
+    writer.add_scalar("Train/rmse", score, epoch)
+    score = evaluate(model, ml1m_dataset.next_batch(128, False))
+    print score
+    writer.add_scalar("Test/rmse", score, epoch)
+
